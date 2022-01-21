0c6b5390addf76ea6555ca6a0b48f916d1b5641b
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-01-21 19:56:12 +0000

    added Register form && reset input

--- src/components/pages/AnotherProject.js
+++ src/components/pages/AnotherProject.js
@@ -31,6 +31,36 @@ export default function AnotherProject() {
         <p>Ordering {number} items</p>
       </>
   }
+
+  function RegisterForm(){
+    const[name, setName] = useState("");
+    const[type, setType] = useState("");
+    const[motivation, setMotivation] = useState("");
+  
+
+  function handleFormSubmit(event){
+    event.preventDefault();
+    setName("");
+    setType("");
+    setMotivation("");
+  }
+  return <>
+    <form onSubmit={handleFormSubmit}>
+      <input type="text" name="name" value={name} onChange={e => setName(e.target.value)} placeholder="Enter your name" />
+      <select name="type" value={type} onChange={e => setType(e.target.value)}>
+          <option>Choose a type</option>
+          <option>Client</option>
+          <option>Merchant</option>
+      </select>
+      <textarea name="motivation" value={motivation} onChange={e => setMotivation(e.target.value)} placeholder="Your motivation to join"/>
+      <input type="submit" value="Register" />
+    </form>
+    <p>
+      Your name is {name}. You would like to register as {type} because {motivation}.
+    </p>
+  </>
+
+  }
   
   return (
 
@@ -45,8 +75,9 @@ export default function AnotherProject() {
     <ControledComponent />
     <h1>Form number</h1>
     <ControledComponentNumber />
-
-
+    
+    <h1>Submit form</h1>
+    <RegisterForm />
 
 
     </div>
