5f0e1d3b1a78ebd9cca0aa6eba47f5ebde287fd8
LA_rmarti11 <LA_rmarti11@PT-1KR86J3.emea.media.global.loc>
2022-02-05 17:57:44 +0000

    Created new stopwatch and refactored clock

--- src/components/pages/Marketing.js
+++ src/components/pages/Marketing.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
 
-export default function Clock() {
-
+export default function Clocks() {
+  function Clock(){
   const [date, setDate] = useState(new Date())
   useEffect(() => {
     const intervalId = setInterval(()=>{
@@ -12,6 +12,54 @@ export default function Clock() {
       clearInterval(intervalId);
     }
   },[])
-  return <h1>{date.toLocaleTimeString()}</h1>
 
-}
+
+  return <>
+    <h1>{date.toLocaleTimeString()}</h1>
+  </>  
+
+  } 
+
+  function StopWatch(){
+    const [counter, setCounter] = useState(0);
+    const [running, setRunning] = useState(false);
+
+    useEffect(()=>{
+      if(running){
+        const timerId = setTimeout(()=>{
+          setCounter(prevCounter => prevCounter + 1)
+        },1000);
+        return () => {
+          clearTimeout(timerId)
+        }
+      }
+    })
+    function handleToggleClick(){
+      setRunning(prevRunning => !prevRunning)
+    }
+    function handleStopClick(){
+      setCounter(0);
+      setRunning(false)
+    }
+    return <>
+    <h2>{counter}</h2>
+    <button onClick={handleToggleClick}>Start / Pause</button>
+    <button onClick={handleStopClick}>Stop</button>
+    </>
+  }
+
+  return(
+  
+    <section className="clock-project">
+      <div className="wrapper">
+        <Clock />
+        <StopWatch />
+      </div>
+    </section>
+  )
+}  
+
+
+
+
+
