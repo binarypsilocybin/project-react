e70ad1b0038a26d815208a44f33ce9af125f6cf5
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-02-09 10:26:27 +0000

    new store locator

--- src/components/pages/Consulting.js
+++ src/components/pages/Consulting.js
@@ -6,29 +6,48 @@ import '../../App.css';
 export default function StoresMap() {
   
   function App() {
-    const [random, setRandom] = useState(Math.random());
+    const [marker, setMarker] = useState();
 
-      mapboxgl.accessToken = "pk.eyJ1IjoiYmluYXJ5cHNpbG9jeWJvbiIsImEiOiJja2p6eTVicjAwYnk5MnhydWV2ZnViZjloIn0.RfJVq5ViwYCYDKdFFE6uCg";
+    mapboxgl.accessToken = "pk.eyJ1IjoiYmluYXJ5cHNpbG9jeWJvbiIsImEiOiJja2p6eTVicjAwYnk5MnhydWV2ZnViZjloIn0.RfJVq5ViwYCYDKdFFE6uCg";
 
     useLayoutEffect(() => {
         const map = new mapboxgl.Map({
             container: 'map', // matches <div id="map" />
             style: 'mapbox://styles/mapbox/dark-v10', // sets the dark mode
-            center: [ -9.13200, 38.72271 ], // Sets the center of the map (long, lat)
-            zoom: 6
+            center:  [ 12.567898, 55.67583 ],
+            zoom: 9
         });
+
+        const mapMarker = new mapboxgl.Marker()
+        .setLngLat([12.567898, 55.67583])
+        .addTo(map);
+        setMarker(mapMarker);
+
 
     },[])
     const stores = {
-       central: [12.567898, 55.67583],
-        norrebro: [12.553806, 55.699299],
-        airport: [12.650784, 55.618042]
-    }
+      central: [12.567898, 55.67583],
+      norrebro: [12.553806, 55.699299],
+      airport: [12.650784, 55.618042]
+  }
 
-      return <>
-        <button id="re-render" onClick={() => setRandom(Math.random())}>Re-render</button>
-        <div id="map"></div>
-    </>;
+    function handleStoreChange(event) {
+      const location = event.target.value;
+      marker.setLngLat(stores[location]);
+  }
+
+
+  return <>
+  <div className="map-overlay">
+      <h3>Choose store: </h3>
+      <select onChange={handleStoreChange}>
+          <option value="central">Central station</option>
+          <option value="norrebro">Norrebro street</option>
+          <option value="airport">CPH Airport</option>
+      </select>
+  </div>
+  <div id="map"></div>
+</>;
 }
  
 
