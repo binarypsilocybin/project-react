6a473f8915b22b354ae9e2c931b25145a32ca032
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-04-25 18:45:07 +0100

    Wallet component

---
+++ src/components/Wallet.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import {render} from "react-dom";
+
+export default function Wallet() {
+    const [transactions, setTransactions] = useState([]);
+
+    function handleDepositClick() {
+        setTransactions([...transactions, transactions.length + 1]);
+    }
+
+    function handleUndoClick() {
+         setTransactions(transactions.slice(0, transactions.length -1))
+    }
+
+    return <>
+        <button onClick={handleDepositClick}>Deposit</button>
+        <button onClick={handleUndoClick}>Undo</button>
+        <ul>
+            {transactions.map(
+                (transaction, index) => <li key={index}>{transaction}</li>
+            )}
+        </ul>
+    </>;
+}
+
--- src/components/pages/Promises.js
+++ src/components/pages/Promises.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ShoppingWallet from '../ShoppingWallet';
+import Wallet from '../Wallet';
 
 export default function Promises() {
 
   return (
     <section className="another-project">
       <div className="wrapper">
-       <ShoppingWallet />´
-       
+       <ShoppingWallet />
+       <Wallet />
+
       </div>
     </section>  
     )
