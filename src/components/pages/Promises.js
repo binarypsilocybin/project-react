43c910bd3b2b96c28e9b6994233a6ad5b1665e50
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-05-06 09:58:00 +0100

    Cleaning component

--- src/components/pages/Promises.js
+++ src/components/pages/Promises.js
@@ -8,10 +8,12 @@ export default function Promises() {
   return (
     <section className="another-project">
       <div className="wrapper">
-       <ShoppingWallet />
-       <Wallet />
-       <h1>Todo App</h1>
-       <TodoApp />
+        <h1>Shopping List Wallet</h1>
+        <ShoppingWallet />
+        <Wallet />
+        <h1>Todo App</h1>
+        <p>Lifting state up</p>
+        <TodoApp />
       </div>
     </section>  
     )
