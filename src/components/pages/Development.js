e7baf1c2ac69fb93c3be294ea2d0fab127496b0c
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-05-12 20:07:40 +0100

    Async fetch and lazy loading

--- src/components/pages/Development.js
+++ src/components/pages/Development.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { lazy, Suspense, useReducer, useState } from "react";
 import Loader from "../../components/Loader";
-import "../../App.css";
+const RiverInformation = lazy(() =>
+  import(/* webpackChunkName: "RiverInformation" */ "../RiverInformation")
+);
 
 export default function Development() {
   const [users, setUsers] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [river, setRiver] = useState("nile");
+  const [show, toggle] = useReducer((state) => !state, true);
 
   function handleLoadUsers() {
     setIsLoading(true);
@@ -21,16 +25,28 @@ export default function Development() {
       });
   }
 
+  const renderUsers = (
+    <ul>{users && users.map((user) => <li key={user.id}>{user.login}</li>)}</ul>
+  );
+
   return (
     <>
       <button onClick={handleLoadUsers} disabled={isLoading}>
         Load Users
       </button>
       <h1>Users</h1>
-      {isLoading && <Loader />}
-      <ul>
-        {users && users.map((user) => <li key={user.id}>{user.login}</li>)}
-      </ul>
+      {isLoading ? <Loader /> : renderUsers}
+      <h1>World's Longest Rivers</h1>
+      <div>
+        <button onClick={toggle}>Toggle Details</button>
+      </div>
+      <button onClick={() => setRiver("nile")}>Nile</button>
+      <button onClick={() => setRiver("amazon")}>Amazon</button>
+      <button onClick={() => setRiver("yangtze")}>Yangtze</button>
+      <button onClick={() => setRiver("mississippi")}>Mississippi</button>
+      <Suspense fallback={<div>Loading Component</div>}>
+        {show && <RiverInformation name={river} />}
+      </Suspense>
     </>
   );
 }
